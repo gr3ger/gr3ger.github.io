e719e38cefb49e917257ae92961059c39f4709ef
Jonas Henriksson <gr3ger@gmail.com>
2021-11-12 14:24:24 +0100

    Create elderFrenzy.js

---
+++ elderFrenzy.js
@@ -0,0 +1,25 @@
+Game.Win('Third-party');
+if(ElderFrenzyBank === undefined) var ElderFrenzyBank = {};
+if(typeof CCSE == 'undefined') Game.LoadMod('https://klattmose.github.io/CookieClicker/' + (0 ? 'Beta/' : '') + 'CCSE.js');
+ElderFrenzyBank.name = 'Elder Frenzy Bank';
+ElderFrenzyBank.version = '1.0';
+ElderFrenzyBank.GameVersion = '2.031';
+
+ElderFrenzyBank.launch = function(){
+  ElderFrenzyBank.init = function(){
+    Game.customStatsMenu.push(function(){
+        CCSE.AppendStatsGeneral("Bank needed "+Beautify((Game.cookiesPsRaw * 60 * 30 * 777) / 0.15));
+      });
+  }
+}
+
+if(!ElderFrenzyBank.isLoaded){
+	if(CCSE && CCSE.isLoaded){
+		ElderFrenzyBank.launch();
+	}
+	else{
+		if(!CCSE) var CCSE = {};
+		if(!CCSE.postLoadHooks) CCSE.postLoadHooks = [];
+		CCSE.postLoadHooks.push(ElderFrenzyBank.launch);
+	}
+}
