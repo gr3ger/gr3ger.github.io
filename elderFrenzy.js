1698d603956021a1da20f9cedbf5ad854cb6b257
Jonas Henriksson <gr3ger@gmail.com>
2021-11-16 09:59:10 +0100

    Update elderFrenzy.js

--- elderFrenzy.js
+++ elderFrenzy.js
@@ -2,23 +2,34 @@ Game.Win('Third-party');
 if(ElderFrenzyBank === undefined) var ElderFrenzyBank = {};
 if(typeof CCSE == 'undefined') Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE.js');
 ElderFrenzyBank.name = 'Elder Frenzy Bank';
-ElderFrenzyBank.version = '1.0';
+ElderFrenzyBank.version = '1.0.1';
 ElderFrenzyBank.GameVersion = '2.031';
 
 ElderFrenzyBank.launch = function(){
     ElderFrenzyBank.isLoaded = 1;
-    Game.customStatsMenu.push(function(){
-	    
-	var rawCps30min = Game.cookiesPsRaw * 60 * 30;
-	var rawCps2h = Game.cookiesPsRaw * 60 * 60 * 2;
-	    
+    Game.customStatsMenu.push(function(){	    
 	CCSE.AppendStatsVersionNumber(ElderFrenzyBank.name, ElderFrenzyBank.version);
-	CCSE.AppendStatsGeneral(`<div class="listing"><b>Conjure Baked Goods + current (de)buffs :</b> ${Beautify(Math.min(Game.cookiesPs * 60 * 30, Game.cookies * 0.15))}</div>`);
-        CCSE.AppendStatsGeneral(`<div class="listing"><b>Elder Frenzy + Conjure Baked Goods :</b> ${Beautify(Math.min(rawCps30min * 666, Game.cookies * 0.15))}</div>`);	    
-        CCSE.AppendStatsGeneral(`<div class="listing"><b>Elder Frenzy + Conjure Baked Goods at max bank :</b> ${Beautify(rawCps30min * 666)} <small>(Bank of ${Beautify((rawCps30min * 666) / 0.15)} needed)</small></div>`);
-        CCSE.AppendStatsGeneral(`<div class="listing"><b>Elder Frenzy + Duketater at max bank :</b> ${Beautify(rawCps2h * 666)} <small>(Bank of ${Beautify((rawCps2h * 666) / 0.08)} needed)</small></div>`);     
-        CCSE.AppendStatsGeneral(`<div class="listing"><b>Elder Frenzy + Bakeberry at max bank :</b> ${Beautify(rawCps30min * 666)} <small>(Bank of ${Beautify((rawCps30min * 666) / 0.03)} needed)</small></div>`);
     });
+	
+    Game.customMinigame["Wizard tower"].spellTooltip.push(function(id, str){
+	if(id === 0){
+	    var rawCps30min = Game.cookiesPsRaw * 60 * 30;
+	    var gain = Beautify(Math.min(Game.cookiesPs * 60 * 30, Game.cookies * 0.15));
+	    var maxGain = Beautify(rawCps30min * 666);
+	    var maxBank = Beautify((rawCps30min * 666) / 0.15);
+
+	    return str.replace('</div></div>', `<div style=\"height:8px;\"></div>
+	    <b>Current: </b> ${gain}
+	    <div></div>
+	    <b>Max (Elder Frenzy): </b> ${maxGain}
+	    <div></div>
+	    <b>Bank needed for max: </b> <span style=\"color:${Game.cookies < maxBank ? "#F00" : "#0F0"}\">${maxBank}
+	    </div></div>`);
+	}
+	else {
+	    return str;
+	}
+    })
 }
 
 if(!ElderFrenzyBank.isLoaded){
