ca87941ff33ff51c7c277efd861e6a5a257c11e8
Jonas Henriksson <gr3ger@gmail.com>
2021-11-22 18:32:32 +0100

    bump to 1.0.2

--- elderFrenzy.js
+++ elderFrenzy.js
@@ -2,7 +2,7 @@ Game.Win('Third-party');
 if (ElderFrenzyBank === undefined) var ElderFrenzyBank = {};
 if (typeof CCSE == 'undefined') Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE.js');
 ElderFrenzyBank.name = 'Elder Frenzy Bank';
-ElderFrenzyBank.version = '1.0.1';
+ElderFrenzyBank.version = '1.0.2';
 ElderFrenzyBank.GameVersion = '2.031';
 
 ElderFrenzyBank.launch = function() {
